c9e3da1b3d25867f22884ad0278dce35db8e1a59
Kai David Albornoz Madrigal <kai.albornoz@udea.edu.co>
2021-04-15 21:13:44 -0500

    Correción de ortografía

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const interface= require('readline-sync');
-var calculator= require('./App/calculator');
+var calculator= require('./App/calculator.js');
 
 const firstNum= interface.question("Deme el primero numero: ");
 const secondNum= interface.question("Deme el segundo numero: ");
